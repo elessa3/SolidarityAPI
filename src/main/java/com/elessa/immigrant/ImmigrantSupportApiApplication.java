c2b906fc95c5eaf18e56727329581a2e80e68eb1
¨elessa3¨ <ericasorriso3@gmail.com>
2026-04-07 14:01:02 +0200

    merge changes

---
+++ src/main/java/com/elessa/immigrant/ImmigrantSupportApiApplication.java
@@ -0,0 +1,12 @@
+package com.elessa.immigrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Esta anotação faz tudo funcionar
+public class ImmigrantSupportApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmigrantSupportApiApplication.class, args);
+    }
+}
