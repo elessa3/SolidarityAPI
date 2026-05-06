cd59f85f6c6ee41ddbeac05ede553faf443fb821
¨elessa3¨ <ericasorriso3@gmail.com>
2026-05-06 11:45:07 +0200

    ajust swagger

--- src/main/java/com/elessa/immigrant/config/SecurityConfig.java
+++ src/main/java/com/elessa/immigrant/config/SecurityConfig.java
@@ -41,7 +41,6 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs",
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
